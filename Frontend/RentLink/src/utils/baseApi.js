49f8d87dfb8c77c3166d906724776dc971b6e4f2
Ranajit Bangal <ranajitbangal01@gmail.com>
2025-09-01 11:21:00 +0530

    Update baseApi.js

--- Frontend/RentLink/src/utils/baseApi.js
+++ Frontend/RentLink/src/utils/baseApi.js
@@ -1,4 +1,4 @@
 // src/utils/baseApi.js
 
-const baseApi =  'http://localhost:3000' // 'https://rentlink.onrender.com/'
+const baseApi = 'https://rentlink.onrender.com' // 'http://localhost:3000' // 'https://rentlink.onrender.com/'
 export default baseApi;
